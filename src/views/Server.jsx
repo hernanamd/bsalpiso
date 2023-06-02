04f943365c3992f935ba35e909e73f6686edd154
unknown <almadahernan16@gmail.com>
2023-06-01 22:15:06 -0300

    update v3

--- src/views/Server.jsx
+++ src/views/Server.jsx
@@ -20,23 +20,23 @@ function Server() {
                 }
             </ul>
 
-            <a href="https://discord.gg/PNpwFJaSGn" className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-blue-800 border-blue-800 hover:bg-blue-800 hover:text-white transition-all'>
+            <a href="https://discord.gg/PNpwFJaSGn" target='blank' className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-blue-800 border-blue-800 hover:bg-blue-800 hover:text-white transition-all'>
                 DISCORD
             </a>
 
-            <a href="https://www.instagram.com/buenosairesalpiso_24_7" className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-pink-500 border-pink-500 hover:bg-pink-600 hover:text-white transition-all'>
+            <a href="https://www.instagram.com/buenosairesalpiso_24_7" target='blank' className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-pink-500 border-pink-500 hover:bg-pink-600 hover:text-white transition-all'>
                 INSTAGRAM
             </a>
 
-            <a href="https://www.facebook.com/groups/BuenosAiresalPisoMtaByBorisYGogeta" className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-blue-500 border-blue-500 hover:bg-blue-600 hover:text-white transition-all'>
+            <a href="https://www.facebook.com/groups/BuenosAiresalPisoMtaByBorisYGogeta" target='blank' className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-blue-500 border-blue-500 hover:bg-blue-600 hover:text-white transition-all'>
                 FACEBOOK
             </a>
 
-            <a href="https://www.tiktok.com/@buenosaresalpiso247mta" className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-teal-300 border-teal-300 hover:bg-teal-300 hover:text-white transition-all'>
+            <a href="https://www.tiktok.com/@buenosaresalpiso247mta" target='blank' className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-teal-300 border-teal-300 hover:bg-teal-300 hover:text-white transition-all'>
                 TIK TOK
             </a>
 
-            <a href="https://chat.whatsapp.com/HMiqsCzkmg626WiKb3n6rY" className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-green-500 border-green-500 hover:bg-green-600 hover:text-white transition-all'>
+            <a href="https://chat.whatsapp.com/HMiqsCzkmg626WiKb3n6rY" target='blank' className='flex justify-center items-center p-4 w-1/2 text-2xl border-2 text-green-500 border-green-500 hover:bg-green-600 hover:text-white transition-all'>
                 WHATSAPP
             </a>
 
